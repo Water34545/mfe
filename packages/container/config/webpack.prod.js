9381f019b1dba8ef7312253aa8883cd13a197955
water <water34545@yandex.ru>
2022-01-28 17:50:55 +0300

    added marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFeferationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
       },
       shared: packageJson.dependencies
     })
