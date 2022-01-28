96a22b70d2a8d8b775a02607cda010695b1e3052
water <water34545@yandex.ru>
2022-01-28 18:04:05 +0300

    new feature

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,5 +2,9 @@ import React from 'react';
 import MacketingApp from './components/MacketingApp';
 
 export default () => {
-  return <MacketingApp/>
+  return <div>
+    <h1>hi!</h1>
+    <hr />
+    <MacketingApp/>
+  </div>
 }
