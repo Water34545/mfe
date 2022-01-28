3759593879c24b82cf3d7309ef1230528655ab36
water <water34545@yandex.ru>
2022-01-28 19:12:46 +0300

    add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import MacketingApp from './components/MacketingApp';
+import Header from './components/Header';
 
 export default () => {
-  return <div>
-    <h1>hi!</h1>
-    <hr />
+  return <BrowserRouter>
+    <Header/>
     <MacketingApp/>
-  </div>
+  </BrowserRouter>
 }
