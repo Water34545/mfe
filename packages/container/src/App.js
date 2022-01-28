52b85c4c7c201d5aba9cab3b27d9dcc1ff905aa1
water <water34545@yandex.ru>
2022-01-28 19:46:31 +0300

    add generateClassName

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 import MacketingApp from './components/MacketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
+
 export default () => {
   return <BrowserRouter>
-    <Header/>
-    <MacketingApp/>
+    <StylesProvider generateClassName={generateClassName}>
+      <Header/>
+      <MacketingApp/>
+    </StylesProvider>
   </BrowserRouter>
 }
