c07c97b7426cadf940609c067e563fef20104902
water <water34545@yandex.ru>
2022-01-29 16:13:37 +0300

    adds dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,32 +1,46 @@
-import React, {lazy, Suspense, useState} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React, {lazy, Suspense, useState, useEffect} from 'react';
+import {Router, Route, Switch, Redirect} from 'react-router-dom';
 import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
+import {createBrowserHistory} from "history";
 import Header from './components/Header';
 import Progress from './components/Progress';
 
 const MacketingLazy = lazy(() => import('./components/MacketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignin, setIsSignin] = useState(false);
 
+  useEffect(() => {
+    if (isSignin) {
+      history.push("/dashboard");
+    }
+  }, [isSignin])
+
   return <StylesProvider generateClassName={generateClassName}>
-    <BrowserRouter>
+    <Router history={history}>
       <Header isSignin={isSignin} onSignOut={() => setIsSignin(false)}/>
       <Suspense fallback={<Progress/>}>
         <Switch>
           <Route path="/auth">
             <AuthLazy onSignIn={() => setIsSignin(true)}/>
           </Route>
+          <Route path="/dashboard">
+            {!isSignin && <Redirect to="/"/>}
+            <DashboardLazy/>
+          </Route>
           <Route path="/">
             <MacketingLazy/>
           </Route>
         </Switch>
       </Suspense>
-    </BrowserRouter>
+    </Router>
   </StylesProvider>
 }
